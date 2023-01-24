cf729916f1a7fc7b5d0a7d87021ab05417bc9b6b
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2023-01-24 17:58:44 -0500

    Updated link

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -5,7 +5,7 @@ type Props = {}
 const Logo = (props: Props) => {
   return (
     <span className="text-3xl font-bold">
-      <Link href="https://www.github.com/aingarant">aingaran</Link></span>
+      <Link href="https://www.twitter.com/aingaran">aingaran</Link></span>
   )
 }
 
